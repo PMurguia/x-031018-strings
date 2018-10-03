f7ef4ecb300fd233081ad894e089fe10bdef65be
Peio Murguia <peiomurguia@gmail.com>
2018-10-03 17:47:33 +0200

    Inic

---
+++ x-031018-strings/x-031018-strings/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace x_031018_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escribe un programa que te calcule la largura de un string.
+
+            //string respuesta;
+            //respuesta = Console.ReadLine();
+
+
+            //Console.WriteLine(respuesta.Length);
+            //Console.ReadLine();
+
+
+            //Escribe un programa que te calcule el total de palabras que hay en un string.
+
+            string respuesta;
+            respuesta = Console.ReadLine();
+
+            Console.WriteLine(respuesta.());
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
