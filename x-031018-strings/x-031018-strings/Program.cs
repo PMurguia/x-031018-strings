55392cecfd9d5433788a5832ac30502afe2c13bc
Peio Murguia <peiomurguia@gmail.com>
2018-10-04 13:17:38 +0200

    Subida Strings

--- x-031018-strings/x-031018-strings/Program.cs
+++ x-031018-strings/x-031018-strings/Program.cs
@@ -31,21 +31,160 @@ namespace x_031018_strings
 
             //Escribe un programa que nos cuente el número de vocales en un string.
 
-            int vocales = 0;
-            string respuesta;
-            Console.WriteLine("Dime tu nombre. ");
-            respuesta = Console.ReadLine();
+            //int vocales = 0;
+            //string respuesta;
+            //Console.WriteLine("Dime tu nombre. ");
+            //respuesta = Console.ReadLine();
 
-            for (int i = 0; i < respuesta.Length; i++)
-            {
-                if(respuesta[i] == 'a' || respuesta[i] == 'e' || respuesta[i] == 'i' || respuesta[i] == 'o' || respuesta[i] == 'u')
-                {
-                    vocales++;
-                }
-                
-            }
-            Console.WriteLine("La palabra " + respuesta + " tiene " + vocales + " vocales.");
+            //for (int i = 0; i < respuesta.Length; i++)
+            //{
+            //    if(respuesta[i] == 'a' || respuesta[i] == 'e' || respuesta[i] == 'i' || respuesta[i] == 'o' || respuesta[i] == 'u')
+            //    {
+            //        vocales++;
+            //    }
+
+            //}
+            //Console.WriteLine("La palabra " + respuesta + " tiene " + vocales + " vocales.");
+            //Console.ReadLine();
+
+
+            //Escribe un programa que te calcule el total de palabras que hay en un string.
+
+            //int palabras = 0;
+
+            //string respuesta;
+            //Console.WriteLine("Dime una frase.");
+            //respuesta = Console.ReadLine();
+
+            //for (int i = 0; i < respuesta.Length; i++)
+            //{
+            //    if (respuesta[i] == ' ')
+            //    {
+            //        palabras++;
+            //    }
+            //}
+            //Console.WriteLine("La frase " + respuesta + " tiene " + (palabras + 1) + " palabras. ");
+            //Console.ReadLine();
+
+
+
+            //Escribe un programa que copia un string a otro string
+
+
+            //Console.WriteLine("Copiemos un string en otro. Dame una palabra. ");
+            //string str11 = Console.ReadLine();
+
+
+            //string str12 = string.Copy(str11);
+            //Console.WriteLine(str12 + "\t" + str11);
+            //Console.ReadLine();
+
+
+
+            //Escribe un programa que devuelva un string en minúsculas en mayúsculas y al revés.
+
+
+            //Console.WriteLine("Convirtamos una palabra en minúsculas a mayúsculas y viceversa");
+            //string str21 = Console.ReadLine();
+
+
+            //Console.WriteLine(str21.ToUpper());
+            //Console.WriteLine(str21.ToLower());
+            //Console.ReadLine();
+
+            //Escribe un programa que evalúe si un string contiene la letra ‘r’
+
+            //Console.WriteLine("Veamos si la palabra tiene la letra 'r'. ");
+            //string str31 = Console.ReadLine().ToLower();
+
+            //if (str31.Contains('r'))
+            //{
+            //    Console.WriteLine("Esta palabra contiene la letra 'r'");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Esta palabra no contiene la letra 'r'");
+            //}
+            //Console.ReadLine();
+
+
+            //Escribe un programa el que dada una frase sacará su carácter central. Nota:
+            //Si la largura de la frase es par deberá sacar los dos caracteres centrales correspondientes.
+            //Si es impar, sólo un carácter.
+
+
+            //Console.WriteLine("Dame una frase y saquemos su carácter central. ");
+            //string str41 = Console.ReadLine();
+            //int large = str41.Length;
+
+
+            //if(large % 2 == 1)
+            //{
+            //    Console.WriteLine("La " + str41[large / 2]);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(str41[(large/2)-1] + " y " + str41[(large/2)]);
+            //}
+            //Console.ReadLine();
+
+
+            //Escribe un programa el cual analizará si la contraseña introducida es correcta o no
+            //Para que la contraseña sea correcta deberá cumplir estas directivas:
+            //Debe tener 8 caracteres como mínimo.
+            //La contraseña deberá contener números.
+            //Deberá contener al menos dos dígitos.
+
+            //AGUA
+
+
+            //Escribe un programa que al meter tres números por teclado identifique el mínimo y el máximo (usa el método correspondiente). 
+
+            int n1, n2, n3;
+            Console.WriteLine("Dame tres números. ");
+            n1 = Int32.Parse(Console.ReadLine());
+            n2 = Int32.Parse(Console.ReadLine());
+            n3 = Int32.Parse(Console.ReadLine());
+
+            int max = Math.Max(n1, n2);
+            int min = Math.Min(n1, n2);
+
+
+            Console.WriteLine("El máximo es: " + Math.Max(max, n3));
+            Console.WriteLine("El mínimo es: " + Math.Min(min, n3));
             Console.ReadLine();
+            
+
+            //if (n1 > n2 && n1 > n3)
+            //{
+            //    Console.WriteLine("El máximo de esos tres números es " + n1);
+            //}
+            //else if (n2 > n1 && n2 > n3)
+            //{
+            //    Console.WriteLine("El máximo de esos tres números es " + n2);
+            //}
+            //else if (n3 > n1 && n3 > n2)
+            //{
+            //    Console.WriteLine("El máximo de esos tres números es " + n3);
+            //}
+            //Console.ReadLine();
+            //Console.WriteLine();
+
+
+            //Escribe un programa que muestre la fecha de hoy. 
+
+            Console.WriteLine("¿Qué día es hoy?");
+            Console.WriteLine("Hora y Fecha: {0}\n", DateTime.Now);
+            Console.ReadLine();
+
+
+
+
         }
+
+
+
+
     }
 }
+
